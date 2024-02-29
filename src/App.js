bef4a264a83a9afd79b213588aefdda356a1f0ef
Abhishek Singh <singh2811abhishek@gmail.com>
2024-02-29 10:03:08 +0530

    bg

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-lignt">
       <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>} />
