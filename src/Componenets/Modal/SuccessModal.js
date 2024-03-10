f064bd3cb7f9e120db154f3c8fd5a07e0b4b6b4c
Abhishek Singh <singh2811abhishek@gmail.com>
2024-03-10 16:47:19 +0530

    change

--- src/Componenets/Modal/SuccessModal.js
+++ src/Componenets/Modal/SuccessModal.js
@@ -5,7 +5,7 @@ const SuccessModal = ({ show }) => {
     <div className={`modal ${show ? 'show' : ''}`} tabIndex="-1" role="dialog" style={{ display: show ? 'block' : 'none'}}>
       <div className="modal-dialog" role="document">
         <div className="modal-content">
-          <div className="modal-header btn-success text-white">
+          <div className="modal-header bg-success text-white">
             <h5 className="modal-title">Login Successful</h5>
             <button type="button" className="btn-close" aria-label="Close"></button>
           </div>
